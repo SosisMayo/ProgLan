6c82d0cb76e37d75690177cc351425ce2f93ac37
SosisMayo <ramadani.dwicahya@gmail.com>
2021-05-18 21:43:07 +0700

    Membuat box sebagai obstacle dan hitboxnya

--- Bom-Boman/main.cpp
+++ Bom-Boman/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <graphics.h>
+#include <math.h>
+//TO DO LIST :
+//1. Benarkan Character
+//2. Grid
+//3. Bikin Musuh
 
 int GetKeyPressed(int nKey)
 {
@@ -10,6 +15,18 @@ int GetKeyPressed(int nKey)
               KeyPreesed =-1;
         }
         return  KeyPreesed;
+}
+
+void Box(float xpos,float ypos,float skala){
+    float x[5],y[5];
+    x[0]=-4;y[0]=-0.5;
+    x[1]=-4;y[1]=12.5;
+    x[2]=4;y[2]=12.5;
+    x[3]=4;y[3]=-0.5;
+    x[4]=-4;y[4]=-0.5;
+    for(int i=0;i<4;i++){
+        line(x[i]*skala+xpos,y[i]*skala+ypos,x[i+1]*skala+xpos,y[i+1]*skala+ypos);
+        }
 }
 
 void bomb(float xpos,float ypos,float skala){
@@ -32,13 +49,13 @@ void bomb(float xpos,float ypos,float skala){
         }
 }
 
-void MainChar(float xpos,float ypos,float skala,int hadap){
+void MainChar(float xpos,float ypos,float skala,int hadap,float n){
     float x[99],y[99];
     //HADAP DEPAN
     if (hadap==83){
         //TOPI
-        x[0]=0;y[0]=-1;
-        x[1]=-2;y[1]=-1;
+        x[0]=0;y[0]=-0.5;
+        x[1]=-2;y[1]=-0.5;
         x[2]=-2;y[2]=0;
         x[3]=-4;y[3]=0;
         x[4]=-4;y[4]=0.5;
@@ -153,8 +170,8 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
     //HADAP BELAKANG
     else if(hadap==87){
         //TOPI
-        x[0]=0;y[0]=-1;
-        x[1]=-2;y[1]=-1;
+        x[0]=0;y[0]=-0.5;
+        x[1]=-2;y[1]=-0.5;
         x[2]=-2;y[2]=0;
         x[3]=-4;y[3]=0;
         x[4]=-4;y[4]=0.5;
@@ -273,8 +290,8 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
     //HADAP KANAN
     else if (hadap==68){
         //TOPI
-        x[0]=0;y[0]=-1;
-        x[1]=-2;y[1]=-1;
+        x[0]=0;y[0]=-0.5;
+        x[1]=-2;y[1]=-0.5;
         x[2]=-2;y[2]=0;
         x[3]=-4;y[3]=0;
         x[4]=-4;y[4]=0.5;
@@ -359,7 +376,7 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
         x[2]=-1;y[2]=11;
         x[3]=-1;y[3]=10.5;
         for(int i=0;i<3;i++){
-            line(x[i]*skala+xpos,y[i]*skala+ypos,x[i+1]*skala+xpos,y[i+1]*skala+ypos);
+            line((x[i]+n)*skala+xpos,y[i]*skala+ypos,(x[i+1]+n)*skala+xpos,y[i+1]*skala+ypos);
         }
 
         //KAKI KIRI
@@ -368,7 +385,7 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
         x[2]=1;y[2]=10.8;
         x[3]=1;y[3]=10.5;
         for(int i=0;i<3;i++){
-            line(x[i]*skala+xpos,y[i]*skala+ypos,x[i+1]*skala+xpos,y[i+1]*skala+ypos);
+            line((x[i]+n)*skala+xpos,y[i]*skala+ypos,(x[i+1]+n)*skala+xpos,y[i+1]*skala+ypos);
         }
 
         //SEPATU KANAN
@@ -385,7 +402,7 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
         x[10]=-0.4;y[10]=11.2;
         x[11]=-1;y[11]=11;
         for(int i=0;i<11;i++){
-            line(x[i]*skala+xpos,y[i]*skala+ypos,x[i+1]*skala+xpos,y[i+1]*skala+ypos);
+            line((x[i]+n)*skala+xpos,y[i]*skala+ypos,(x[i+1]+n)*skala+xpos,y[i+1]*skala+ypos);
         }
 
         //SEPATU KIRI
@@ -402,15 +419,15 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
         x[10]=2.6;y[10]=11.2;
         x[11]=2;y[11]=11;
         for(int i=0;i<11;i++){
-            line(x[i]*skala+xpos,y[i]*skala+ypos,x[i+1]*skala+xpos,y[i+1]*skala+ypos);
+            line((x[i]+n)*skala+xpos,y[i]*skala+ypos,(x[i+1]+n)*skala+xpos,y[i+1]*skala+ypos);
         }
     }
 
     //HADAP KIRI
     else if (hadap==65){
         //TOPI
-        x[0]=0;y[0]=-1;
-        x[1]=-2;y[1]=-1;
+        x[0]=0;y[0]=-0.5;
+        x[1]=-2;y[1]=-0.5;
         x[2]=-2;y[2]=0;
         x[3]=-4;y[3]=0;
         x[4]=-4;y[4]=0.5;
@@ -495,7 +512,7 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
         x[2]=-1;y[2]=11;
         x[3]=-1;y[3]=10.5;
         for(int i=0;i<3;i++){
-            line(-x[i]*skala+xpos,y[i]*skala+ypos,-x[i+1]*skala+xpos,y[i+1]*skala+ypos);
+            line((-x[i]+n)*skala+xpos,y[i]*skala+ypos,(-x[i+1]+n)*skala+xpos,y[i+1]*skala+ypos);
         }
 
         //KAKI KIRI
@@ -504,7 +521,7 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
         x[2]=1;y[2]=10.8;
         x[3]=1;y[3]=10.5;
         for(int i=0;i<3;i++){
-            line(-x[i]*skala+xpos,y[i]*skala+ypos,-x[i+1]*skala+xpos,y[i+1]*skala+ypos);
+            line((-x[i]+n)*skala+xpos,y[i]*skala+ypos,(-x[i+1]+n)*skala+xpos,y[i+1]*skala+ypos);
         }
 
         //SEPATU KANAN
@@ -521,7 +538,7 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
         x[10]=-0.4;y[10]=11.2;
         x[11]=-1;y[11]=11;
         for(int i=0;i<11;i++){
-            line(-x[i]*skala+xpos,y[i]*skala+ypos,-x[i+1]*skala+xpos,y[i+1]*skala+ypos);
+            line((-x[i]+n)*skala+xpos,y[i]*skala+ypos,(-x[i+1]+n)*skala+xpos,y[i+1]*skala+ypos);
         }
 
         //SEPATU KIRI
@@ -538,50 +555,163 @@ void MainChar(float xpos,float ypos,float skala,int hadap){
         x[10]=2.6;y[10]=11.2;
         x[11]=2;y[11]=11;
         for(int i=0;i<11;i++){
-            line(-x[i]*skala+xpos,y[i]*skala+ypos,-x[i+1]*skala+xpos,y[i+1]*skala+ypos);
+            line((-x[i]+n)*skala+xpos,y[i]*skala+ypos,(-x[i+1]+n)*skala+xpos,y[i+1]*skala+ypos);
         }
     }
 }
 
 int main()
 {
-    int hadap=83;
-    float skala=10;
-    static float xpos1,ypos1;
-    float xbomb=-9999,ybomb=-99999;
-    bool IsBomb=false;
+    float xhbBom[2],yhbBom[2],xhbChar[2],yhbChar[2],n=0,xhbBox[99][2],yhbBox[99][2],skala=10,xpos1,ypos1,xbomb=-99,ybomb=-99;
+    int hadap=83,jumlahBox=5,xposBox[99],yposBox[99];
     xpos1 = 100; ypos1 =200;
-    initwindow(500,500);
+    for(int i=0;i<jumlahBox;i++){
+        xposBox[i]=rand()%1920;
+        yposBox[i]=rand()%1080;
+        while((xposBox[i]%2!=0) || (yposBox[i]%2!=0)){
+            xposBox[i]=rand()%1920;
+            yposBox[i]=rand()%1080;
+        }
+        xhbBox[i][0]=(-4*skala)+xposBox[i];
+        xhbBox[i][1]=(4*skala)+xposBox[i];
+        yhbBox[i][0]=(-0.5*skala)+yposBox[i];
+        yhbBox[i][1]=(12.5*skala)+yposBox[i];
+
+    }
+    initwindow(1920,1080);
     while(1)
     {
+        xhbBom[0]=(-4*skala)+xbomb;
+        xhbBom[1]=(4*skala)+xbomb;
+        yhbBom[0]=(-0.5*skala)+ybomb;
+        yhbBom[1]=(12.5*skala)+ybomb;
+        xhbChar[0]=(-4*skala)+xpos1;
+        xhbChar[1]=(4*skala)+xpos1;
+        yhbChar[0]=(-0.5*skala)+ypos1;
+        yhbChar[1]=(12.5*skala)+ypos1;
+        for(int i=0;i<5;i++){
+            Box(xposBox[i],yposBox[i],skala);
+        }
         bomb(xbomb,ybomb,skala);
-        MainChar(xpos1,ypos1,skala,hadap);
+        MainChar(xpos1,ypos1,skala,hadap,n);
         if (GetAsyncKeyState(68)){
             hadap=68;
             xpos1=xpos1+2;
+            if((xhbChar[1]>=xhbBom[0]) && (xhbChar[1]<xhbBom[1]) && (yhbChar[0]>yhbBom[0]) && (yhbChar[0]<yhbBom[1])){
+                xpos1=xpos1-2;
+            }
+            else if((xhbChar[1]>=xhbBom[0]) && (xhbChar[1]<xhbBom[1]) && (yhbChar[1]>yhbBom[0]) && (yhbChar[1]<yhbBom[1])){
+                xpos1=xpos1-2;
+            }
+            else if((xhbChar[1]>=xhbBom[0]) && (xhbChar[1]<xhbBom[1]) && (yhbChar[0]==yhbBom[0]) && (yhbChar[1]==yhbBom[1])){
+                xpos1=xpos1-2;
+            }
             if (xpos1+(4*skala)>getmaxx()){
                 xpos1=getmaxx()-(4*skala);
+            }
+            for(int i=0;i<jumlahBox;i++){
+                if((xhbChar[1]>=xhbBox[i][0]) && (xhbChar[1]<xhbBox[i][1]) && (yhbChar[0]>yhbBox[i][0]) && (yhbChar[0]<yhbBox[i][1])){
+                    xpos1=xpos1-2;
+                }
+                else if((xhbChar[1]>=xhbBox[i][0]) && (xhbChar[1]<xhbBox[i][1]) && (yhbChar[1]>yhbBox[i][0]) && (yhbChar[1]<yhbBox[i][1])){
+                    xpos1=xpos1-2;
+                }
+                else if((xhbChar[1]>=xhbBox[i][0]) && (xhbChar[1]<xhbBox[i][1]) && (yhbChar[0]==yhbBox[i][0]) && (yhbChar[1]==yhbBox[i][1])){
+                    xpos1=xpos1-2;
+                }
+            }
+            if(n==0){
+                n=0.5;
+            }
+            else{
+                n=0;
             }
         }
         else if (GetAsyncKeyState(65)){
             hadap=65;
             xpos1=xpos1-2;
+            if((xhbChar[0]>xhbBom[0]) && (xhbChar[0]<=xhbBom[1]) && (yhbChar[0]>yhbBom[0]) && (yhbChar[0]<yhbBom[1])){
+                xpos1=xpos1+2;
+            }
+            else if((xhbChar[0]>xhbBom[0]) && (xhbChar[0]<=xhbBom[1]) && (yhbChar[1]>yhbBom[0]) && (yhbChar[1]<yhbBom[1])){
+                xpos1=xpos1+2;
+            }
+            else if((xhbChar[0]>xhbBom[0]) && (xhbChar[0]<=xhbBom[1]) && (yhbChar[0]==yhbBom[0]) && (yhbChar[1]==yhbBom[1])){
+                xpos1=xpos1+2;
+            }
             if (xpos1+(-4*skala)<0){
                 xpos1=4*skala;
+            }
+            for(int i=0;i<jumlahBox;i++){
+                if((xhbChar[0]>xhbBox[i][0]) && (xhbChar[0]<=xhbBox[i][1]) && (yhbChar[0]>yhbBox[i][0]) && (yhbChar[0]<yhbBox[i][1])){
+                    xpos1=xpos1+2;
+                }
+                else if((xhbChar[0]>xhbBox[i][0]) && (xhbChar[0]<=xhbBox[i][1]) && (yhbChar[1]>yhbBox[i][0]) && (yhbChar[1]<yhbBox[i][1])){
+                    xpos1=xpos1+2;
+                }
+                else if((xhbChar[0]>xhbBox[i][0]) && (xhbChar[0]<=xhbBox[i][1]) && (yhbChar[0]==yhbBox[i][0]) && (yhbChar[1]==yhbBox[i][1])){
+                    xpos1=xpos1+2;
+                }
+            }
+            if(n==0){
+                n=-0.5;
+            }
+            else{
+                n=0;
             }
         }
         else if (GetAsyncKeyState(87)){
             hadap=87;
             ypos1=ypos1-2;
-            if (ypos1+(-1*skala)<0){
-                ypos1=1*skala;
+            if((yhbChar[0]>yhbBom[0]) && (yhbChar[0]<=yhbBom[1]) && (xhbChar[0]>xhbBom[0]) && (xhbChar[0]<xhbBom[1])){
+                ypos1=ypos1+2;
+            }
+            else if((yhbChar[0]>yhbBom[0]) && (yhbChar[0]<=yhbBom[1]) && (xhbChar[1]>xhbBom[0]) && (xhbChar[1]<xhbBom[1])){
+                ypos1=ypos1+2;
+            }
+            else if((yhbChar[0]>yhbBom[0]) && (yhbChar[0]<=yhbBom[1]) && (xhbChar[0]==xhbBom[0]) && (xhbChar[1]==xhbBom[1])){
+                ypos1=ypos1+2;
+            }
+            if (ypos1+(-0.5*skala)<0){
+                ypos1=0.5*skala;
+            }
+            for(int i=0;i<jumlahBox;i++){
+                if((yhbChar[0]>yhbBox[i][0]) && (yhbChar[0]<=yhbBox[i][1]) && (xhbChar[0]>xhbBox[i][0]) && (xhbChar[0]<xhbBox[i][1])){
+                    ypos1=ypos1+2;
+                }
+                else if((yhbChar[0]>yhbBox[i][0]) && (yhbChar[0]<=yhbBox[i][1]) && (xhbChar[1]>xhbBox[i][0]) && (xhbChar[1]<xhbBox[i][1])){
+                    ypos1=ypos1+2;
+                }
+                else if((yhbChar[0]>yhbBox[i][0]) && (yhbChar[0]<=yhbBox[i][1]) && (xhbChar[0]==xhbBox[i][0]) && (xhbChar[1]==xhbBox[i][1])){
+                    ypos1=ypos1+2;
+                }
             }
         }
         else if (GetAsyncKeyState(83)){
             hadap=83;
             ypos1=ypos1+2;
+            if((yhbChar[1]>=yhbBom[0]) && (yhbChar[1]<yhbBom[1]) && (xhbChar[0]>xhbBom[0]) && (xhbChar[0]<xhbBom[1])){
+                ypos1=ypos1-2;
+            }
+            else if((yhbChar[1]>=yhbBom[0]) && (yhbChar[1]<yhbBom[1]) && (xhbChar[1]>xhbBom[0]) && (xhbChar[1]<xhbBom[1])){
+                ypos1=ypos1-2;
+            }
+            else if((yhbChar[1]>=yhbBom[0]) && (yhbChar[1]<yhbBom[1]) && (xhbChar[0]==xhbBom[0]) && (xhbChar[1]==xhbBom[1])){
+                ypos1=ypos1-2;
+            }
             if (ypos1+(12.5*skala)>getmaxy()){
                 ypos1=getmaxy()-(12.5*skala);
+            }
+            for(int i=0;i<jumlahBox;i++){
+                if((yhbChar[1]>=yhbBox[i][0]) && (yhbChar[1]<yhbBox[i][1]) && (xhbChar[0]>xhbBox[i][0]) && (xhbChar[0]<xhbBox[i][1])){
+                    ypos1=ypos1-2;
+                }
+                else if((yhbChar[1]>=yhbBox[i][0]) && (yhbChar[1]<yhbBox[i][1]) && (xhbChar[1]>xhbBox[i][0]) && (xhbChar[1]<xhbBox[i][1])){
+                    ypos1=ypos1-2;
+                }
+                else if((yhbChar[1]>=yhbBox[i][0]) && (yhbChar[1]<yhbBox[i][1]) && (xhbChar[0]==xhbBox[i][0]) && (xhbChar[1]==xhbBox[i][1])){
+                    ypos1=ypos1-2;
+                }
             }
         }
         else if (GetKeyPressed(32)){
@@ -589,7 +719,7 @@ int main()
             ybomb=ypos1;
             continue;
         }
-        else if (GetKeyPressed(27)){
+         else if (GetKeyPressed(27)){
             break;
         }
         delay(10);
